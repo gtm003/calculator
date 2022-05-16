4b37b3f57d280931976d4718be0ed8e4d8238d57
Tatyana Grigorovich <gtm003@gmail.com>
2022-05-16 10:07:58 +0300

    Add eslint rules

--- src/calculator.ts
+++ src/calculator.ts
@@ -14,11 +14,13 @@ interface ICalculator {
 }
 
 const strToNumber = (str: string, action: string) => {
-  if (str[0] === "-" && action === "-") {
+  if (str[0] === '-' && action === '-') {
     const args = [`-${str.split(action)[1]}`, str.split(action)[2]];
-    return args.map((operand) => Number(operand.replace(",", ".")));
+
+    return args.map((operand) => Number(operand.replace(',', '.')));
   }
-  return str.split(action).map((operand) => Number(operand.replace(",", ".")));
+
+  return str.split(action).map((operand) => Number(operand.replace(',', '.')));
 };
 
 const getFontSize = (strLength: number) => {
@@ -26,6 +28,7 @@ const getFontSize = (strLength: number) => {
     window.screen.width < breakPoint
       ? window.screen.width - 60
       : breakPoint - 80;
+
   return Math.min(
     Math.ceil((display2Width / strLength) * 1.6),
     window.screen.width < breakPoint ? fontSizeSmall : fontSizeBig
@@ -40,21 +43,21 @@ export class Calculator implements ICalculator {
   constructor(displayNode1: HTMLElement, displayNode2: HTMLElement) {
     (this.display1 = displayNode1),
       (this.display2 = displayNode2),
-      (this.currentOperand = ""),
-      (this.currentOperation = "");
+      (this.currentOperand = ''),
+      (this.currentOperation = '');
   }
 
   clear() {
-    this.display1.innerText = "";
-    this.display2.innerText = "0";
-    this.currentOperand = "";
+    this.display1.innerText = '';
+    this.display2.innerText = '0';
+    this.currentOperand = '';
     this.display2.style.fontSize = `${
       window.screen.width < breakPoint ? fontSizeSmall : fontSizeBig
     }px`;
   }
 
   appendNumber(number: string) {
-    if (number === "," && this.currentOperand.includes(",")) {
+    if (number === ',' && this.currentOperand.includes(',')) {
       return;
     }
     if (/=|√/.test(this.currentOperation)) {
@@ -65,59 +68,65 @@ export class Calculator implements ICalculator {
       this.currentOperand.length
     )}px`;
     this.display2.innerText = this.currentOperand;
-    this.currentOperation = "";
+    this.currentOperation = '';
   }
 
   openBracket() {
     if (this.currentOperand) {
-      this.display1.innerText += this.currentOperand + "x(";
-      this.currentOperand = "";
+      this.display1.innerText += this.currentOperand + 'x(';
+      this.currentOperand = '';
+
       return;
     }
-    this.display1.innerText += "(";
+    this.display1.innerText += '(';
   }
 
   closeBracket() {
-    const numberOfOpeningBrackets = this.display1.innerText.split("(").length;
-    const numberOfClosingBrackets = this.display1.innerText.split(")").length;
+    const numberOfOpeningBrackets = this.display1.innerText.split('(').length;
+    const numberOfClosingBrackets = this.display1.innerText.split(')').length;
+
     if (
       this.currentOperation ||
       numberOfOpeningBrackets <= numberOfClosingBrackets
     ) {
       return;
     }
-    this.display1.innerText += this.currentOperand + ")";
-    this.currentOperand = "";
+    this.display1.innerText += this.currentOperand + ')';
+    this.currentOperand = '';
   }
 
   chooseOperation(operation: string) {
-    if (this.currentOperation === "=") {
+    if (this.currentOperation === '=') {
       this.currentOperation = operation;
-      if (operation === "√") {
+      if (operation === '√') {
         this.display1.innerText = operation + this.currentOperand;
       } else {
         this.display1.innerText = this.currentOperand + operation;
       }
-      this.currentOperand = "";
+      this.currentOperand = '';
+
       return;
     }
-    if (operation === "√") {
+    if (operation === '√') {
       if (!this.currentOperand) {
         const display1 = this.display1.innerText;
-        const insertionIndex: number = display1.lastIndexOf("(");
+        const insertionIndex: number = display1.lastIndexOf('(');
+
         this.display1.innerText = `${display1.slice(
           0,
           insertionIndex
         )}√${display1.slice(insertionIndex)}`;
-        this.currentOperand = "";
+        this.currentOperand = '';
+
         return;
       }
       this.currentOperation = operation;
       this.display1.innerText += `√(${this.currentOperand})`;
-      this.currentOperand = "";
+      this.currentOperand = '';
+
       return;
     }
-    if (this.currentOperation === "" || this.currentOperation === "√") {
+    if (this.currentOperation === '' || this.currentOperation === '√') {
       this.currentOperation = operation;
       this.display1.innerText += this.currentOperand + operation;
     } else {
@@ -125,58 +134,66 @@ export class Calculator implements ICalculator {
       this.display1.innerText =
         this.display1.innerText.slice(0, -1) + this.currentOperand + operation;
     }
-    this.currentOperand = "";
+    this.currentOperand = '';
   }
 
   calculate = (str: string, action: string) => {
     let res;
+
     switch (action) {
-      case "*":
+      case '*':
         res = strToNumber(str, action)[0] * strToNumber(str, action)[1];
         break;
-      case "x":
+      case 'x':
         res = strToNumber(str, action)[0] * strToNumber(str, action)[1];
         break;
-      case "/":
+      case '/':
         res = strToNumber(str, action)[0] / strToNumber(str, action)[1];
         break;
-      case "+":
+      case '+':
         res = strToNumber(str, action)[0] + strToNumber(str, action)[1];
         break;
-      case "-":
+      case '-':
         res = strToNumber(str, action)[0] - strToNumber(str, action)[1];
         break;
       default:
         break;
     }
+
     return String(res?.toFixed(10))
-      .replace(".", ",")
-      .replace(/\,?0*$/, "");
+      .replace('.', ',')
+      .replace(/\,?0*$/, '');
   };
 
   doFirstActions(str: string): string {
     let newStr = str;
     let targetExp = firstActionRegExp.exec(newStr);
+
     while (targetExp) {
       const expr = targetExp[0];
       const action = targetExp[2];
       const res = this.calculate(expr, action);
+
       newStr = newStr.replace(expr, String(res));
       targetExp = firstActionRegExp.exec(newStr);
     }
+
     return newStr;
   }
 
   doSecondActions = (str: string) => {
     let newStr = str;
     let targetExp = secondActionRegExp.exec(newStr);
+
     while (targetExp) {
       const expr = targetExp[0];
       const action = targetExp[2];
       const res = this.calculate(expr, action);
+
       newStr = newStr.replace(expr, String(res));
       targetExp = secondActionRegExp.exec(newStr);
     }
+
     return newStr;
   };
 
@@ -184,45 +201,53 @@ export class Calculator implements ICalculator {
     let newStr = str;
     let count = 0;
     let targetExp = bracketsRegExp.exec(newStr);
+
     while (targetExp && count < 3) {
       const expr: string = targetExp[0].slice(1, -1);
       const res = this.doSecondActions(this.doFirstActions(expr));
+
       newStr = newStr.replace(targetExp[0], String(res));
       targetExp = bracketsRegExp.exec(newStr);
       count++;
     }
+
     return newStr;
   };
   defineRoots = (str: string) => {
     let newStr = str;
     let count = 0;
     let targetExp = rootRegExp.exec(newStr);
+
     while (targetExp && count < 3) {
-      const arg: string = targetExp[0].replace("√", "");
+      const arg: string = targetExp[0].replace('√', '');
       const res = Math.sqrt(Number(arg)).toFixed(10);
+
       newStr = newStr.replace(targetExp[0], String(res));
       targetExp = rootRegExp.exec(newStr);
       count++;
     }
+
     return newStr;
   };
 
   calculateExp = () => {
     const currentExpression: string =
-      this.display1.textContent + this.currentOperand ?? "";
+      this.display1.textContent + this.currentOperand ?? '';
     const withoutBrackets = this.openBrackets(currentExpression);
     const withoutRoots = this.defineRoots(withoutBrackets);
     const firstStep = this.doFirstActions(withoutRoots);
     const secondStep = this.doSecondActions(firstStep);
-    this.display1.innerText += this.currentOperand + "=";
-    this.currentOperation = "=";
-    if (isFinite(Number(secondStep.replace(",", ".")))) {
+
+    this.display1.innerText += this.currentOperand + '=';
+    this.currentOperation = '=';
+    if (isFinite(Number(secondStep.replace(',', '.')))) {
       this.display2.style.fontSize = `${getFontSize(secondStep.length)}px`;
       this.currentOperand = secondStep;
       this.display2.innerText = secondStep;
+
       return;
     }
-    this.display2.innerText = "ERROR";
-    this.currentOperand = "";
+    this.display2.innerText = 'ERROR';
+    this.currentOperand = '';
   };
 }
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
-import { Calculator } from "./calculator";
-import "./style.scss";
+import { Calculator } from './calculator';
+import './style.scss';
 
-const app = document.querySelector<HTMLDivElement>("#app")!;
+const app = document.querySelector<HTMLDivElement>('#app')!;
 
 const createElement = (
   tagName: string,
@@ -9,6 +9,7 @@ const createElement = (
   textContent: string
 ) => {
   const element = document.createElement(tagName);
+
   if (classList) classList.forEach((item) => element.classList.add(item));
   if (textContent) element.textContent = textContent;
 
@@ -17,90 +18,104 @@ const createElement = (
 
 interface IButton {
   value: string;
-  type: "number" | "operation" | "equal" | "clear";
+  type: 'number' | 'operation' | 'equal' | 'clear';
 }
 
 const arr: IButton[] = [
-  { value: "c", type: "clear" },
-  { value: "√", type: "operation" },
-  { value: "%", type: "operation" },
-  { value: "/", type: "operation" },
-  { value: "7", type: "number" },
-  { value: "8", type: "number" },
-  { value: "9", type: "number" },
-  { value: "x", type: "operation" },
-  { value: "4", type: "number" },
-  { value: "5", type: "number" },
-  { value: "6", type: "number" },
-  { value: "-", type: "operation" },
-  { value: "1", type: "number" },
-  { value: "2", type: "number" },
-  { value: "3", type: "number" },
-  { value: "+", type: "operation" },
-  { value: "00", type: "number" },
-  { value: "0", type: "number" },
-  { value: ",", type: "number" },
-  { value: "=", type: "equal" },
+  { value: 'c', type: 'clear' },
+  { value: '√', type: 'operation' },
+  { value: '%', type: 'operation' },
+  { value: '/', type: 'operation' },
+  { value: '7', type: 'number' },
+  { value: '8', type: 'number' },
+  { value: '9', type: 'number' },
+  { value: 'x', type: 'operation' },
+  { value: '4', type: 'number' },
+  { value: '5', type: 'number' },
+  { value: '6', type: 'number' },
+  { value: '-', type: 'operation' },
+  { value: '1', type: 'number' },
+  { value: '2', type: 'number' },
+  { value: '3', type: 'number' },
+  { value: '+', type: 'operation' },
+  { value: '00', type: 'number' },
+  { value: '0', type: 'number' },
+  { value: ',', type: 'number' },
+  { value: '=', type: 'equal' },
 ];
 
-const container = document.createElement("div");
-container.classList.add("container");
-app?.appendChild(container);
+const container = document.createElement('div');
 
-const display1 = document.createElement("div");
-display1.classList.add("display1");
-container?.appendChild(display1);
+container.classList.add('container');
+app.appendChild(container);
 
-const display2 = document.createElement("div");
-display2.classList.add("display2");
-display2.innerText = "0";
-container?.appendChild(display2);
+const display1 = document.createElement('div');
+
+display1.classList.add('display1');
+container.appendChild(display1);
+
+const display2 = document.createElement('div');
+
+display2.classList.add('display2');
+display2.innerText = '0';
+container.appendChild(display2);
 
 const calculator = new Calculator(display1, display2);
 
-const controls = document.createElement("div");
-controls.classList.add("controls");
-container?.appendChild(controls);
+const controls = document.createElement('div');
+
+controls.classList.add('controls');
+container.appendChild(controls);
 
 arr.forEach((item) => {
-  const button = createElement("button", ["button"], String(item.value));
+  const button = createElement('button', ['button'], String(item.value));
+
   switch (item.type) {
-    case "clear":
-      button.addEventListener("click", () => calculator.clear());
+    case 'clear':
+      button.addEventListener('click', () => calculator.clear());
       break;
-    case "operation":
-      button.addEventListener("click", () =>
+    case 'operation':
+      button.addEventListener('click', () =>
         calculator.chooseOperation(button.innerText)
       );
       break;
-    case "number":
-      button.addEventListener("click", () =>
-        calculator.appendNumber(button.innerText)
-      );
+    case 'number':
+      button.addEventListener('click', (event: any) => {
+        if (event.pointerId === -1) {
+          return;
+        }
+        calculator.appendNumber(button.innerText);
+      });
       break;
-    case "equal":
-      button.addEventListener("click", () => calculator.calculateExp());
+    case 'equal':
+      button.addEventListener('click', () => calculator.calculateExp());
       break;
 
     default:
       break;
   }
-  controls?.appendChild(button);
+  controls.appendChild(button);
 });
 
-window.addEventListener("keyup", (event) => {
+window.addEventListener('keyup', (event) => {
   if (/\d|\.|\,/.test(event.key)) {
-    const arg = event.key === "." ? "," : event.key;
+    const arg = event.key === '.' ? ',' : event.key;
+
     calculator.appendNumber(arg);
   }
   if (/\+|\-|\/|\*|x/.test(event.key)) {
-    const operation = event.key === "*" ? "x" : event.key;
+    const operation = event.key === '*' ? 'x' : event.key;
+
     calculator.chooseOperation(operation);
   }
-  if (event.key === "(") {
+  if (event.key === '(') {
     calculator.openBracket();
   }
-  if (event.key === ")") {
+  if (event.key === ')') {
     calculator.closeBracket();
+  }
+  if (event.key === '=' || event.key === 'Enter') {
+    event.preventDefault();
+    calculator.calculateExp();
   }
 });
